281a6ef200b0d2d89588d2f1f39b93140cc08f38
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-11-15 13:30:21 +0000

    Added dbsetup to main and removed single quotes from sql statement

--- src/hospitaladministration/DatabaseSetup.java
+++ src/hospitaladministration/DatabaseSetup.java
@@ -14,23 +14,24 @@ import java.sql.Statement;
  * @author Lecturer
  */
 public class DatabaseSetup {
-    final String DB_BASE_URL = "jdbc:mysql://localhost";
-    final String USER = "ooc2023";
-    final String PASSWORD = "ooc2023";
+    final static String DB_BASE_URL = "jdbc:mysql://localhost";
+    final static String USER = "ooc2023";
+    final static String PASSWORD = "ooc2023";
     
-    public boolean setupDB() throws SQLException {
+    public static boolean setupDB() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         try(
                 Connection conn = DriverManager.getConnection(DB_BASE_URL, USER, PASSWORD);
                 Statement stmt = conn.createStatement();  
             ){
-            stmt.execute("CREATE DATABASE IF NOT EXISTS 'hospital';");
-            stmt.execute("USE 'hospital';");
+            stmt.execute("CREATE DATABASE IF NOT EXISTS hospital;");
+            stmt.execute("USE hospital;");
             String sql = 
-                    "CREATE TABLE IF NOT EXISTS 'patientData' ("
-                    + "'name' VARCHAR(255),"
-                    + "'birthdate' DATE,"
-                    + "'bloodType' VARCHAR(2),"
-                    + "'id' INT(10)"
+                    "CREATE TABLE IF NOT EXISTS patientData ("
+                    + "name VARCHAR(255),"
+                    + "birthdate DATE,"
+                    + "bloodType VARCHAR(2),"
+                    + "id INT(10)"
                     + ");";
             stmt.execute(sql);
             return true;
--- src/hospitaladministration/HospitalAdministration.java
+++ src/hospitaladministration/HospitalAdministration.java
@@ -4,6 +4,8 @@
  */
 package hospitaladministration;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author Lecturer
@@ -13,7 +15,7 @@ public class HospitalAdministration {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         Patient p1 = new Patient("Sam", "15/11/23", "O+");
         Patient p2 = new Patient("Lexie", "27/11/21", "A-");
         
@@ -21,6 +23,11 @@ public class HospitalAdministration {
         System.out.println(p2.getPatientID());
         
         System.out.println(Patient.getCurrentID());
+        if (DatabaseSetup.setupDB()) {
+            System.out.println("Database and table created");
+        } else {
+            System.out.println("Oh no! There was a database creation problem...");
+        }
     }
     
 }
