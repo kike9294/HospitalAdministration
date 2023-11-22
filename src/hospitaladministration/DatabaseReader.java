aafb038c951c8520c2b2f0e258eb810982e959f8
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-11-22 14:08:01 +0000

    Exercise

--- src/hospitaladministration/DatabaseReader.java
+++ src/hospitaladministration/DatabaseReader.java
@@ -12,17 +12,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
  * @author Lecturer
  */
 public class DatabaseReader extends Database {
-    public void getAllData() {
+    public ArrayList<Patient> getAllData() {
         try(
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
             Statement stmt = conn.createStatement();  
         ){
+            /*
+            Exercise
+            read each row
+            assign the data to a new Patient object
+            add the Patient to the list
+            return the list
+            */
             // Object that stores the result of our query
             ResultSet results = stmt.executeQuery(String.format(
                     "SELECT * FROM %s;",
@@ -37,6 +45,7 @@ public class DatabaseReader extends Database {
             // Selects row 3
             System.out.println(results.next());
             // Selects row 4
+            System.out.println(results.next());
             System.out.println(results.next());
             System.out.println(results.getString("name"));
             System.out.println(results.getInt("id"));
